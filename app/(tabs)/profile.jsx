4b10e2e693f96d73758627ee7259be62b533d14b
Akalanka Dias <101487259+AKALANKA28@users.noreply.github.com>
2024-08-10 02:14:31 +0530

    settings

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,10 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import UserIntro from "../../components/Profile/UserIntro";
+import MenuList from "../../components/Profile/MenuList";
 
 export default function profile() {
   return (
-    <View>
-      <Text>profile</Text>
+    <View styel={{ padding: 20 }}>
+      <Text
+        style={{
+          fontSize: 35,
+          fontFamily: "roboto-bold",
+          textTransform: "capitalize",
+        }}
+      >
+        profile
+      </Text>
+
+      {/* User Info */}
+      <UserIntro/>
+  
+      {/* Menu Item */}
+      <MenuList/>
     </View>
-  )
+  );
 }
