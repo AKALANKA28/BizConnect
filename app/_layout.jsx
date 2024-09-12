e779d4154c33df5a1275ce93c1b32512470c284f
Akalanka Dias <101487259+AKALANKA28@users.noreply.github.com>
2024-09-13 02:25:28 +0530

    update

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,6 +2,7 @@ import { View, Text } from "react-native";
 import React, { useEffect } from "react";
 import { Slot, useRouter, useSegments } from "expo-router";
 import { AuthContextProvider, useAuth } from "../context/authContext";
+import { useFonts } from "expo-font";
 
 const MainLayout = () => {
   const { isAuthenticated } = useAuth();
@@ -24,10 +25,28 @@ const MainLayout = () => {
 };
 
 export default function RootLayout() {
-  console.log('Root layout is loading');
+  // console.log('Root layout is loading');
 
+  const [fontsLoaded] = useFonts({
+    'roboto': require('../assets/fonts/Roboto-Regular.ttf'),
+    'roboto-medium': require('../assets/fonts/Roboto-Medium.ttf'),
+    'roboto-bold': require('../assets/fonts/Roboto-Bold.ttf'),
+    'poppins': require('../assets/fonts/Poppins-Regular.ttf'),
+    'poppins-semibold': require('../assets/fonts/Poppins-SemiBold.ttf'),
+    'poppins-bold': require('../assets/fonts/Poppins-Bold.ttf'),
+    'roboto-black': require('../assets/fonts/Roboto-Black.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
   return (
     <AuthContextProvider>
+  
       <MainLayout />
     </AuthContextProvider>
   );
